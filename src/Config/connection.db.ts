191523e07639132dbdae619973cb58af1eff9995
imprint <asiwebrightemeka@gmail.com>
2023-05-30 01:27:12 +0100

    fixed config vars

--- src/Config/connection.db.ts
+++ src/Config/connection.db.ts
@@ -2,34 +2,6 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 import { taskEntity } from "src/task/task.entity";
 import { AuthEntity } from "src/auth/auth.entity";
 
-// export const connectionConfig: TypeOrmModuleOptions = {
-
-//   type: 'postgres',
-//   host:
-//     process.env.NODE_ENV === 'production'
-//       ? process.env.PGHOST
-//       :"localhost",
-//   port:
-//     process.env.NODE_ENV === 'production'
-//       ? +process.env.PGPORT
-//       :  5432,
-//   username:
-//     process.env.NODE_ENV === 'production'
-//       ? process.env.PGUSER
-//       : 'postgres',
-//   password:
-//     process.env.NODE_ENV === 'production'
-//       ? process.env.PGPASSWORD
-//       : 'imprint',
-//   database:
-//     process.env.NODE_ENV === 'production'
-//       ? process.env.PGDATABASE
-//       : 'messagemanager',
-//   synchronize: process.env.NODE_ENV === 'development',
-//     // entities: [taskEntity, AuthEntity],
-//   entities:[__dirname+"../**/*.entity.ts" ],
-// };
-
 export const connectionConfig: TypeOrmModuleOptions =
 
   process.env.NODE_ENV === 'production' ? {
